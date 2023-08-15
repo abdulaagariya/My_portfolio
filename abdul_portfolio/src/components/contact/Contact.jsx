6fbb19d2d59b5a5ce67830b324eb733ff8909d7f
Abdul <d22it181@charusat.edu.in>
2023-08-15 23:10:45 +0530

    initial commit

---
+++ abdul_portfolio/src/components/contact/Contact.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from "react"; // Import useState
+import emailjs from "emailjs-com";
+import { BsArrowRight } from "react-icons/bs";
+import { RiSendPlaneFill } from "react-icons/ri";
+
+const Contact = () => {
+  const form = useRef();
+  const [sent, setSent] = useState(false); // State to track whether the email was sent
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Submit button clicked");
+   
+    try {
+      const result = await emailjs.sendForm(
+        "service_4ki1awp",
+        "template_74pnxae",
+        form.current,
+        "skkXVNkFgEUBKz6rc"
+      );
+      console.log(result.text);
+      setSent(true); // Update the sent state to true
+    } catch (error) {
+      console.log(error.text);
+    }
+  };
+
+  return (
+    <div id="contact" className="container m-auto mt-16">
+
+      {/* heading */}
+      <div 
+      // data-aos="fade-up"
+       className="relative mb-5">
+        <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
+          Contact
+        </h3>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+        <h1 className="text-4xl font-bold sm:text-2xl">You Need</h1>
+        <p className=" text-gray-700 font-medium w-[100%]">
+        Beautiful design for your website leave a request
+        </p>
+      </div>
+
+      <div className="right flex-1">
+
+        
+        <form
+          ref={form}
+          onSubmit={handleSubmit}
+          data-aos="zoom-in"
+          className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[90%] sm:w-[85%] mx-auto"
+
+        >
+          <input type="text" name="user_name" className="user" placeholder="Name" />
+          <input type="text" name="user_email" className="user" placeholder="Email" />
+          <textarea name="message" className="user" placeholder="Message" />
+          <input type="submit" value="Send" className="button" />
+
+          {/* Show a message after sending */}
+          {sent && <p>Email sent successfully!</p>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ abdul_portfolio/src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/0Q5GVfT0/Screenshot-2023-08-11-104327.png',
+    title: "Shop Cart",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/WbqzcZjm/Screenshot-2023-08-01-222105.png',
+    title: "Tarvel Agency",
+    link: "",
+    git: "https://github.com/abdulaagariya/Travel_website",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/2SfkpX0m/Screenshot-2023-08-02-154744.png',
+    title: "Medical web",
+    desc: "",
+    link: "",
+    git: "https://github.com/abdulaagariya/Medical_website",
+  }
+  
+];
